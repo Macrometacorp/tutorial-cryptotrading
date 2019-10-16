7457373ae39b050b5c7205fcea4bdb7b00cfd534
Abhishek Lohani <abhishek.lohani@macrometa.co>
2019-10-16 14:50:07 +0530

    changed file ref

--- global-producers/start_cron.sh
+++ global-producers/start_cron.sh
@@ -3,7 +3,7 @@
 path="/root/tutorial-cryptotrading/global-producers"
 
 # start the node application initially before the cron job
-bash ${path}/run_cron.sh
+bash ${path}/cron_helper.sh
 
 crontab -l > mycron
 echo "0 */6 * * * bash ${path}/cron_helper.sh" >> mycron
